ceba580ca8a1a16880aecf05d08e62a005daec3f
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2022-06-16 16:29:43 +0600

    desing side menu and setup page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Home />
+      </div>
     </>
   );
 }
