b6b66fe6afe32762778108935b71d3358df10ca8
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2022-06-16 23:58:31 +0600

    add chart

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,19 @@
 import React from "react";
+import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
+import { userData } from "../../dummyData";
 
 const Home = () => {
   return (
     <div className="home">
-      <div></div>
       <FeaturedInfo />
+      <Chart
+        grid
+        title="User Analytics"
+        data={userData}
+        dataKey="Active User"
+      />
     </div>
   );
 };
