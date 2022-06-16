b63c3541f62c9d3e7db2f896db369dac3d07c802
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2022-06-16 23:16:04 +0600

    featured Info design added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
 
 const Home = () => {
   return (
     <div className="home">
       <div></div>
-      Home
+      <FeaturedInfo />
     </div>
   );
 };
