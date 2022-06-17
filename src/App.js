32795ed9c7d2ee8030c207e787cd7fc438444710
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2022-06-17 17:09:59 +0600

    added user update feature

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/Home/Home";
 import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/users" element={<UserList />} />
+            <Route path="/user/:userId" element={<User />} />
+            <Route path="/newUser" element={<NewUser />} />
           </Routes>
         </BrowserRouter>
       </div>
